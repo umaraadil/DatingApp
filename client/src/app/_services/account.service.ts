e346e8f3134bd09e01549d96a8f055cf22ad9095
umaraadil <aadil.ddn@gmail.com>
2021-01-03 11:18:49 +0530

    Completed sec 9

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { User } from './../_model/user';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -9,7 +10,7 @@ import { ReplaySubject } from 'rxjs';
 })
 export class AccountService {
 
-  baseUrl='https://localhost:5001/api/';
+  baseUrl=environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
