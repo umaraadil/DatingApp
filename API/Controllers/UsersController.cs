c42d586f54b331b95becf4535507c04d7bd51f14
umaraadil <aadil.ddn@gmail.com>
2021-01-11 13:09:12 +0530

    Done with sec 13

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -30,10 +31,17 @@ namespace API.Controllers
 
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users=await _userRepository.GetMembersAsync();
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            userParams.CurrentUsername = user.UserName;
+            if(string.IsNullOrEmpty(userParams.Gender))
+            userParams.Gender= user.Gender=="male"?"female":"male";
+            var users=await _userRepository.GetMembersAsync(userParams);
+            Response.AddPaginationHeader(users.CurrentPage,
+            users.PageSize,users.TotalCount,users.TotalPages);
             return Ok(users);
+            
         }
         [Authorize]
         //api/users/id
--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -9,8 +9,8 @@ namespace API.DTOs
         public string PhotoUrl { get; set; }
         public int Age { get; set; }
         public string KnownAs { get; set; }
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime LastActive { get; set; } = DateTime.Now;
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
